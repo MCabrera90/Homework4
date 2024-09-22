49ee9bcaf48c516a13cd2ad462ec2f70356894c8
Matheo Cabrera <mcabre24@stu.southtexascollege.edu>
2024-09-22 08:57:23 -0500

    finished balance

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import Header from './components/Header.vue';
+  import Balance from './components/Balance.vue';
+  import { ref, computed } from 'vue';
+
+  const transactions = ref([
+    {id: 1, text: 'Paycheck', amount: 700.00},
+    {id: 1, text: 'Paycheck', amount: 700.00},
+    {id: 1, text: 'Paycheck', amount: -2000.00},
+    {id: 1, text: 'Paycheck', amount: 700.00},
+  ])
+
+  const sum = computed(()=>{
+    return transactions.value.reduce((acc, x)=>{
+      return acc+x.amount
+    },0)
+  })
+</script>
+<template>
+  <Header></Header>
+  <div class="container">
+    <Balance :total="1000"></Balance>
+  </div>    
+</template>
